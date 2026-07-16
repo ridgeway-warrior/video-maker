70a80eb65c04167ea8137b955b19771d92e389b3
Ahmed <ahmed.akod@outlook.com>
2026-07-15 22:52:50 -0400

    fix: add a try and catch to load .env in dev environment

--- src/ai.ts
+++ src/ai.ts
@@ -1,6 +1,7 @@
 import { ChatOpenAI } from "@langchain/openai";
 import { prompt } from "./prompt";
 import { topic } from "./topics";
+import { loadEnvFile } from "process";
 
 const randomTopic = topic.topics[Math.round(Math.random() * (topic.topics.length - 1))]
 const randomStyle = topic.formats[Math.round(Math.random() * (topic.formats.length - 1))]
@@ -8,6 +9,17 @@ console.log(randomStyle, randomTopic)
 const agent = new ChatOpenAI({
     model: "gpt-5.4-mini",
 });
+
+try {
+    loadEnvFile();
+} catch (error) {
+    if (error instanceof Error) {
+        console.log(error.message);
+    }
+    else {
+        console.log(error)
+    }
+}
 
 export async function ridgeway_boss() {
     const output = await agent.invoke(
